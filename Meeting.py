ad2e3258522cf20805a8304530e05a13f5bdd1d8
joverandout <u1917702@live.warwick.ac.uk>
2021-02-05 19:20:02 +0000

    start of the host class

---
+++ Meeting.py
@@ -0,0 +1,35 @@
+import datetime
+import sys
+
+class Meeting():
+    title = ""
+    category = ""
+    code = ""
+    __starttime = None
+    __duration = None
+    __host = None
+    __participatns = None
+    __in_progress = None
+    
+    def __init__(self, title, category, code, startime, duration, host, in_progress):
+        self.title = title
+        self.category = category
+        self.code = code
+        self.__starttime = startime
+        self.__duration = duration
+        self.__host = host
+        self.__participatns = []
+        self.__in_progress = in_progress
+
+    def to_string(self):
+        string = ""
+        string += "| MEETING: "
+        string += self.title
+        string += "\n| Startime: "
+        string += str(self.__starttime)
+        string += "\n| host: "
+        string += self.__host
+        string += "\n| ongoing: "
+        string += str(self.__in_progress)
+        return string
+
