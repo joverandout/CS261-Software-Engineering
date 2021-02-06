29e506d4db002d44738939084f65b4c5a3cc7e3e
nkosi <nkosik11@gmail.com>
2021-02-06 19:03:23 +0000

    proxy sign in page, testing context

--- frontend/src/pages/HostSignIn/index.js
+++ frontend/src/pages/HostSignIn/index.js
@@ -1,13 +1,28 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 
+
 export default function HostSignIn(){
-  return(
-    ReactDOM.render(
-      <h1>Hello, world! - Sign in</h1>,
-      document.getElementById('root')
-    )
+  const [signedIn, setSignedIn] = useState(false);
+
+  function signin(){
+    console.log("Pressed!")
+    setSignedIn(signedIn != true);
+  }
+
+  let signedInIndicator = (<h2>Signed in: {signedIn.toString()}</h2>);
+
+
+  const page = (
+    <div>
+      <h1>Host Sign In Page</h1>
+      {signedInIndicator}
+      <button onClick={signin}>
+        Sign In
+      </button>
+    </div>
   )
+  return page
 }
