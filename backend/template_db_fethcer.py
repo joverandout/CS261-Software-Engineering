253ba552c1a412681b00898b8bf346cc7bd3931d
Joe Moore <60425398+joverandout@users.noreply.github.com>
2021-02-09 22:27:56 +0000

    Update template_db_fethcer.py

--- backend/template_db_fethcer.py
+++ backend/template_db_fethcer.py
@@ -12,7 +12,7 @@ def get_template_from_database(db):
     h1 = Host("user123", "9999", "John", "Smith", "password123")
     p1 = Attendee("attendee123", "1", "name", "name", True)
     p2 = Attendee("attendee124", "2", "Jackie", "Cooper", False)
-    t1 = template("template1", ["happy", "sad"], ["is cereal a soup?"])
+    t1 = Template("template1", ["happy", "sad"], ["is cereal a soup?"])
     m1 = t1.make_new_meetings("Title", "Category", "Code", datetime.now(), datetime.now(), h1, False)
 
     participants = [p1, p2]
