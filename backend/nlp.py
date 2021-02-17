84d3d2f2754901dbd78c7de3fc5f7b093214b00c
Dominika Daraż <42532942+Precel2000@users.noreply.github.com>
2021-02-17 10:23:24 +0000

    citation and commenting improved

--- backend/nlp.py
+++ backend/nlp.py
@@ -1,28 +1,42 @@
+# @inproceedings{akbik2018coling,
+#   title={Contextual String Embeddings for Sequence Labeling},
+#   author={Akbik, Alan and Blythe, Duncan and Vollgraf, Roland},
+#   booktitle = {{COLING} 2018, 27th International Conference on Computational Linguistics},
+#   pages     = {1638--1649},
+#   year      = {2018}
+# }
+
 from flair.models import TextClassifier
 from flair.models import SequenceTagger
 from flair.data import Sentence
 
 # load tagger
-classifier = TextClassifier.load('sentiment')
-# predict for example sentence
+classifier = TextClassifier.load('en-sentiment')
 
-sentence1 = Sentence("That is fantastic")
-sentence2 = Sentence("I've never seen anyone mess up a presentation like this before")
-sentence3 = Sentence("I wish I could leave")
-sentence4 = Sentence("I miss my cats")
+#  example sentences
+sentence1 = Sentence("Overall, design is good and thoroughly describes most of the processes in the system. The analysis section could have included some more detail, as several different avenues of text analysis are described, but no firm decision is made. Methodology seems sensible, and the presented timeline seems reasonable. Having a list of tests is good, but it would have been nice to see a more formal description of each test (including data to use and expected outcomes)")
+sentence2 = Sentence("Your wife should leave you")
 
 
 
-classifier.predict(sentence1)
-classifier.predict(sentence2)
-classifier.predict(sentence3)
-classifier.predict(sentence4)
-
-# check prediction
+# check prediction for first sentence
 print(sentence1)
+# this modifies the sentence to include the sentiment and confidence of analysis
+classifier.predict(sentence1)
+print('Sentence above is: ')
+print(sentence1.labels[0].value)
+print(sentence1.labels[0].score)
+
+
+# print prediction for second sentence
 print(sentence2)
-print(sentence3)
-print(sentence4)
+# this modifies the sentence to include the sentiment and confidence of analysis
+classifier.predict(sentence2)
+print('Sentence above is: ')
+print(sentence2.labels[0].value)
+print(sentence2.labels[0].score)
+
+
 
 
 #setup required
