764fc598361b146a35afa922ffb7b1e2106e9192
nkosi <nkosik11@gmail.com>
2021-03-01 00:07:30 +0000

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import { Router } from 'react-router-dom';
-
-import history from './services/history';
 import Routes from './routes';
 
 import './App.css';
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
-import history from './services/history';
-import Routes from './routes';
+
 
 
 ReactDOM.render((
