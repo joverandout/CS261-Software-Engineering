e774dee62db120039028c4cad0e6f4f67c88956c
nkosi <nkosik11@gmail.com>
2021-03-01 00:06:23 +0000

    supporting code from html translation

---
+++ frontend/src/api/getHostMeetings.js
@@ -0,0 +1,19 @@
+import Axios from 'axios';
+
+export default function getHostMeetings(){
+    return [{
+        eventName:"Event 1",
+        eventTime:" 20/02/21 : 1600",
+        tag:"Tag1"
+    }, 
+    {
+        eventName:"Event 2",
+        eventTime:" 26/02/21 : 1300",
+        tag:"Tag1"
+    }, 
+    {
+        eventName:"Event 3",
+        eventTime:" 11/03/21 : 2200",
+        tag:"Tag3"
+    }]
+}
---
+++ frontend/src/components/emotion_button.js
@@ -0,0 +1,33 @@
+import "../styles.css"
+import "../template_styles.css"
+import React, {useContext, useState, useCallback, useEffect} from 'react';
+
+export default function EmotionButton(props){
+    const [value, setValue] = useState(false)
+    const [btnStyle, setStyle] = useState({backgroundColor:""})
+
+    if(!props.emotion){
+        return null
+    }
+
+
+    let toggleEmotionCb = props.toggleEmotionCb
+    
+    function toggle(){
+        let newVal = (value ? false:true);
+        
+        if(!toggleEmotionCb(props.id, newVal)){
+            return
+        }
+        setValue(newVal)
+        if(newVal){
+            setStyle({backgroundColor: props.emotion.color})
+        }else{
+            setStyle({backgroundColor: ""})
+        }
+    }
+
+    return(
+        <button style={btnStyle} onClick={toggle}>{props.emotion.name}</button>
+    )
+}
