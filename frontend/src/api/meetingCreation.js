74f50c7419da1ad8116a19f29ec05ad0ebe58d47
nkosi <nkosik11@gmail.com>
2021-03-04 15:55:06 +0000

    changed route names

--- frontend/src/api/meetingCreation.js
+++ frontend/src/api/meetingCreation.js
@@ -1,7 +1,7 @@
 import API from "../backendApi";
 
 export default async function meetingCreation(data){
-    return API.post("/meetingcreation", data).then(res=>{
+    return API.post("/newmeeting", data).then(res=>{
         return true
     }).catch(err=>{
         throw new Error("could not create the meeting")
--- frontend/src/api/templateCreation.js
+++ frontend/src/api/templateCreation.js
@@ -1,7 +1,7 @@
 import API from "../backendApi";
 
 export default async function templateCreation(data){
-    return API.post("/templatecreation", data).then(res=>{
+    return API.post("/newtemplate", data).then(res=>{
         return true
     }).catch(err=>{
         throw new Error("Could not create template")
